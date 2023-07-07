888364852966f1ddc12ad3bed82bd8cac6ad064e
Romina Verónica Torres <110548507+RomiTorres@users.noreply.github.com>
2023-07-07 19:31:58 +0000

    Counter v1.0

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,23 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
-// include your styles into the webpack bundle
 import "../styles/index.css";
 
+// include your styles into the webpack bundle
+
+
 //import your own components
-import Home from "./component/home.jsx";
+//import Home from "./component/home.jsx";
+import SecondsCounter from "./component/SecondsCounter.jsx";
+
+let counter = 0;
+setInterval(() => {
+    // render your react application
+    console.log(counter)
+    ReactDOM.render(<SecondsCounter counter={counter}/>, document.querySelector("#app"));
+    // incremento el contador
+    counter++;
+}, 1000);
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
